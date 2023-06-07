a7a770a91f5d88178d040312d5da67f1107e8d7f
matthewmcdonald <matthew.mcdonaldddd@gmail.com>
2023-06-06 23:42:17 -0700

    June 6 commit 2

--- Dashboard/DashboardViews/PlayView.swift
+++ Dashboard/DashboardViews/PlayView.swift
@@ -20,29 +20,79 @@ struct PlayView: View {
                         Rectangle().foregroundColor(Color.white).frame(width: 320, height: 200).cornerRadius(5).shadow(radius: 5)
                         VStack{
                             HStack{
-                                Text("Stroke Play").fontDesign(.rounded).font(.system(size: 30)).bold().foregroundColor(.black)
+                                Text("Stroke Play").fontDesign(.rounded).font(.system(size: 30)).bold().foregroundColor(.black).padding(.bottom, 5)
                                 
                                 NavigationLink(destination: SPInstructions()){
                                     Image(systemName: "info.circle.fill").font(.system(size: 20)).foregroundColor(.gray)
                                 }
-                            }.padding(.trailing, 100).padding(.bottom, 90)
+                                
+                                Spacer()
+                            }
                             
-                            Text("Play for the lowest amount \nof strokes to win").multilineTextAlignment(.leading).padding(.trailing, 80).padding(.bottom, 20)
-                        }
+                            HStack{
+                                Text("Play for the lowest amount of strokes").foregroundColor(.black)
+                                
+                                Spacer()
+                            }
+                            Spacer()
+                            Rectangle().foregroundColor(.black).frame(width: 290, height: 10).cornerRadius(5)
+                            
+                            
+                        }.frame(width: 290, height: 170)
                     }
                 }.padding()
                 
                 NavigationLink(destination: StrokePlayGameView()){
                     ZStack{
                         Rectangle().foregroundColor(Color.white).frame(width: 320, height: 200).cornerRadius(5).shadow(radius: 5)
-                        
+                        VStack{
+                            HStack{
+                                Text("Skins").fontDesign(.rounded).font(.system(size: 30)).bold().foregroundColor(.black).padding(.bottom, 5)
+                                
+                                NavigationLink(destination: SPInstructions()){
+                                    Image(systemName: "info.circle.fill").font(.system(size: 20)).foregroundColor(.gray)
+                                }
+                                
+                                Spacer()
+                            }
+                            
+                            HStack{
+                                Text("Lowest hole score wins the skin").foregroundColor(.black)
+                                
+                                Spacer()
+                            }
+                            Spacer()
+                            Rectangle().foregroundColor(.green).frame(width: 290, height: 10).cornerRadius(5)
+                            
+                            
+                        }.frame(width: 290, height: 170)
                     }
                 }.padding()
                 
                 NavigationLink(destination: StrokePlayGameView()){
                     ZStack{
                         Rectangle().foregroundColor(Color.white).frame(width: 320, height: 200).cornerRadius(5).shadow(radius: 5)
-                        
+                        VStack{
+                            HStack{
+                                Text("Match Play").fontDesign(.rounded).font(.system(size: 30)).bold().foregroundColor(.black).padding(.bottom, 5)
+                                
+                                NavigationLink(destination: SPInstructions()){
+                                    Image(systemName: "info.circle.fill").font(.system(size: 20)).foregroundColor(.gray)
+                                }
+                                
+                                Spacer()
+                            }
+                            
+                            HStack{
+                                Text("Play to win the most holes").foregroundColor(.black)
+                                
+                                Spacer()
+                            }
+                            Spacer()
+                            Rectangle().foregroundColor(.blue).frame(width: 290, height: 10).cornerRadius(5)
+                            
+                            
+                        }.frame(width: 290, height: 170)
                     }
                 }.padding()
                 
